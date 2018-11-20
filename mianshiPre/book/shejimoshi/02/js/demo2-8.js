d2b8ce6097e7c16b008fe962979b26c55dd9b911
huangtao <huangtao_2121@126.com>
2018-11-20 18:16:18 +0800

    继承相关

--- mianshiPre/book/shejimoshi/02/js/demo2-8.js
+++ mianshiPre/book/shejimoshi/02/js/demo2-8.js
@@ -7,7 +7,33 @@ function inheritObject ( o ) {
 }
 // 寄生
 function inheritPrototype (subClass, superClass) {
-  var p = inheritObject(superClass.prototype)；
+  var p = inheritObject(superClass.prototype);
   p.constructor= subClass;
   subClass.prototype = p;
 }
+function SuperClass (name) {
+  this.name = name;
+  this.books = ['html', 'css', 'javascript'];
+}
+SuperClass.prototype.getName = function () {
+  return this.name
+};
+// 声明子类
+function SubClass (name,time) {
+  SuperClass.call(this, name);
+  this.time = time;
+}
+inheritPrototype(SubClass, SuperClass);
+// 子类原型自己的方法
+SubClass.prototype.getTIme = function () {
+  return this.time;
+}
+
+var val = new SubClass('js book', 2016)
+var val1 = new SubClass('js book111', 1989)
+val.books.push('hello');
+console.log(val.books);
+console.log(val1.books);
+console.log(val.getName())
+console.log(val1.getName())
+
