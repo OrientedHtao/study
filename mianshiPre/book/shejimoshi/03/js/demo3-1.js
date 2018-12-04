7f771050682a90b7f5eda7fab491a310ead1ff51
huangtao <huangtao_2121@126.com>
2018-12-04 12:28:31 +0800

    up

--- mianshiPre/book/shejimoshi/03/js/demo3-1.js
+++ mianshiPre/book/shejimoshi/03/js/demo3-1.js
@@ -13,3 +13,20 @@ userNameAlert.show();
 var passwordAlert = new LoginAlert('输入的密码不正确');
 passwordAlert.show();
 // 添加 用户名不存在 提示语
+var loginConfirm = function (text) {
+  this.content = text;
+}
+loginConfirm.prototype.show  = function () {
+  //显示确认框
+  return this.content
+}
+var loginFailConfirm = new loginConfirm('您的用户名不存在，请重新输入');
+loginFailConfirm.show();
+// 添加 登录成功
+var LoginPrompt = function (text) {
+  this.content = text;
+}
+LoginPrompt.prototype.show = function () {
+  //显示
+  return this.content;
+}
